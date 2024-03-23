b87e7504ae4fa39aebbfa3216fee9d91a27a80e9
minthantaungaung <minthant.aungaung.contact@gmail.com>
2024-03-23 14:11:06 +0700

    first commit

---
+++ SyncfusionBlazorApp1/Data/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Syncfusion.Blazor.ImageEditor;
+
+namespace SyncfusionBlazorApp1.Data
+{
+
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		private const string CONNECTIONSTRING = "DefaultEndpointsProtocol=https;AccountName=sitoblob;AccountKey=DfjG5fpdqa5OEzKMAiIINL8CkEuNdL4Dm+z3HHq51bJ2GhJkAoCH/x7q1b16PSM5Tpf4NMhwDPue+ASt4iSaeg==;EndpointSuffix=core.windows.net";
+
+		[HttpPost("[action]")]
+		[Route("api/Home/Save")]
+		public void Save(IList<IFormFile> UploadFiles)
+		{
+			try
+			{
+				foreach (var file in UploadFiles)
+				{
+					if (UploadFiles != null)
+					{
+						using (var ms = new MemoryStream())
+						{
+							file.CopyTo(ms);
+						
+						// Create a memory stream from the edited file
+
+						// Save to Azure
+						string containerName = "sito/rte";
+
+						var blobServiceClient = new BlobServiceClient(CONNECTIONSTRING);
+						var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+						var blobClient = containerClient.GetBlobClient("rte/" + "file");
+
+						// Upload the MemoryStream to Azure Blob Storage
+						var rslt = blobClient.UploadAsync(ms, true).Result;
+
+						ms.Close();
+
+						var pathToImage = "https://sitoblob.blob.core.windows.net/sito/profileImages/";
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Response.Clear();
+				Response.ContentType = "application/json; charset=utf-8";
+				Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
+			}
+		}
+	}
+
+}
---
+++ SyncfusionBlazorApp1/Data/ImageController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+
+namespace SyncfusionBlazorApp1.Data
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ImageController : ControllerBase
+	{
+		[HttpPost("[action]")]
+        public async Task Save(IList<IFormFile> UploadFiles)
+		{
+			try
+			{
+				string blobUrl = "";
+				foreach (var file in UploadFiles)
+				{
+					const string accountName = "sitodev";
+					const string key = "StZmbVEwJyjQVShmtCLv+lTsz0hqRu/+TQKEAm+aKNHHwX9cP3WrdVu5myJkilN0DkaH0dDZYfid+AStoOIwwA==";
+
+					var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, key), true);
+
+					var blobClient = storageAccount.CreateCloudBlobClient();
+					var container = blobClient.GetContainerReference("fileupload"); // Provide your container name
+					await container.CreateIfNotExistsAsync();
+					await container.SetPermissionsAsync(new BlobContainerPermissions()
+					{
+						PublicAccess = BlobContainerPublicAccessType.Blob
+					});
+
+					var blob = container.GetBlockBlobReference(file.FileName);
+					using (var stream = file.OpenReadStream())
+					{
+						await blob.UploadFromStreamAsync(stream);
+					}
+					blobUrl = blob.Uri.ToString();
+				}
+				Response.StatusCode = 200;
+			}
+			catch (Exception e)
+			{
+				Response.Clear();
+				Response.StatusCode = 204;
+				Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "File failed to upload";
+				Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
+			}
+		}
+	}
+}
